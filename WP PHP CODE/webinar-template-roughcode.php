2548bfd978e260bafe80de372d832f66a7b9b434
muheeb hussain <muheebhussain14@gmail.com>
2022-12-15 18:13:31 +0500

    12/15/2022 6:13am

--- WP PHP CODE/webinar-template-roughcode.php
+++ WP PHP CODE/webinar-template-roughcode.php
@@ -1,46 +1,6 @@
- 
 
 
-
-<?php  
-    $datetime = new DateTime();
-    $today = $datetime->format('Ymd');
-    $mainarray = array(
-        'post_type' => array('webinar'),
-        'post_status' => array('publish'),
-        'orderby' => 'meta_value_num',
-        'order' => 'DESC',
-        'posts_per_page' => 4,
-        'meta_query' => array(
-            array(
-                'key' => 'select_date',
-                'value' => $today,
-                'type' => 'numeric',
-                'compare' => '>='
-            )
-        ),
-    );
-	$argsarray = array(
-		'post_type' => array('webinar'),
-		'post_status' => array('publish'),
-		'orderby' => 'meta_value_num',
-		'order' => 'DESC',
-		'posts_per_page' => 4,
-		'meta_query' => array(
-			array(
-				'key' => 'select_date',
-				'value' => $today,
-				'type' => 'numeric',
-				'compare' => '<='
-			)
-		),
-	);
-    $q = new WP_Query($mainarray);
-	$ps = new WP_Query($argsarray);
-?>
-
-<div class="upcoming-webinar">
-    <table class="webinar-table hp-upcoming-webinar-table ">
+<table class="webinar-table hp-upcoming-webinar-table ">
         <thead>
             <tr>
                 <th class="table-title">Presentation Title</th>
@@ -49,77 +9,102 @@
 				<th class="table-title"></th>
             </tr>
         </thead>
-        <tbody>
-			</tbody>
-    </table>
-			<div class="owl-carousel">
-            <?php if($q->have_posts()): while ($q->have_posts()) : $q->the_post(); ?>
-            <div class="webinar-content-row">
-				<div class="webinar-inner">
-					<div class="uw-content">
-						<h4 class="upcoming-webinar-haeding"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-						<p>
-							<?php if(!empty(get_the_excerpt())){
-								echo strip_tags(substr(get_the_excerpt(), 0, 200));
-							}
-							else{
-								echo strip_tags(substr(get_the_content(), 0, 200));
-							} ?>
-						</p>
-					</div>
-					<div class="uw-date">
-						<p class="table-text">TBD 2023</p>
-					</div>
-					<div class="uw-text">
-						<p class="table-text"><?php the_field('presenter_name'); ?>
-								<?php the_field('presenter_title'); ?>
-						</p>
-					</div>
-					<div class="uw-btn">
-						<p class="table-btn"><a href="<?php the_permalink(); ?>">Register ➜</a></p>
-					</div>
-    			</div>
-			</div>
-            <?php endwhile; 
-				elseif($ps->have_posts()): while ($ps->have_posts()) : $ps->the_post(); ?>
-             <div class="webinar-content-row">
-				<div class="webinar-inner">
-					<div class="uw-content">
-						<h4 class="upcoming-webinar-haeding"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-						<p>
-							<?php if(!empty(get_the_excerpt())){
-								echo strip_tags(substr(get_the_excerpt(), 0, 200));
-							}
-							else{
-								echo strip_tags(substr(get_the_content(), 0, 200));
-							} ?>
-						</p>
-					</div>
-					<div class="uw-date">
-						<p class="table-text">TBD 2023</p>
-					</div>
-					<div class="uw-text">
-						<p class="table-text"><?php the_field('presenter_name'); ?>
-								<?php the_field('presenter_title'); ?>
-						</p>
-					</div>
-					<div class="uw-btn">
-						<p class="table-btn"><a href="<?php the_permalink(); ?>">Register ➜</a></p>
-					</div>
-    			</div>
-			</div>
-            <?php endwhile; ?>				
-			<div>
-				
-				<table class="webinar-table hp-upcoming-webinar-table ">
-        <thead>
-			<?php else: ?>
-				
-            <tr>
-                <th class="table-record-heading">No Record Found!</th>
-                <script>jQuery(document).ready(function(){ jQuery(document).find('.custom-btn').hide(); });</script>
-            </tr>
-            <?php endif; wp_reset_postdata(); ?>
-        </tbody>
     </table>
 </div>
+
+
+
+
+<?php  
+    // PAGINATION CODE
+    $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+    $mainarray = array(
+        'post_type' => array('post'),
+        'post_status' => array('publish'),
+        'orderby' => 'date',
+        'order' => 'DESC',
+        'posts_per_page' => 9,
+        'paged'       => $paged,
+    );
+    $q = new WP_Query($mainarray); 
+    ?>
+
+    <div class="custom-main-blog-wrapper">
+        <div class="container">
+            <div class="owl-carousel-blog owl-carousel">
+
+                <!-- Loop Start -->
+
+                <?php while ($q->have_posts()) : $q->the_post(); ?>
+
+                <?php 
+                    if(has_post_thumbnail()):
+                        $thumbnail = wp_get_attachment_image_src(get_post_thumbnail_id(),'large', true);
+                    endif;
+                ?>
+
+                <div class="slide">
+
+				<div class="webinar-content-row">
+    <div class="webinar-inner">
+        <div class="uw-content">
+            <h4 class="upcoming-webinar-haeding"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+           <p>
+			   <?php if(!empty(get_the_excerpt())){
+					echo strip_tags(substr(get_the_excerpt(), 0, 200));
+				}
+			   else{
+				   echo strip_tags(substr(get_the_content(), 0, 200));
+			   } ?>
+			</p>
+        </div>
+        <div class="uw-date">
+            <p class="table-text">TBD 2023</p>
+        </div>
+        <div class="uw-text">
+            <p class="table-text"><?php the_field('presenter_name'); ?>
+					<?php the_field('presenter_title'); ?>
+            </p>
+        </div>
+        <div class="uw-btn">
+            <p class="table-btn"><a href="<?php the_permalink(); ?>">Register ➜</a></p>
+        </div>
+    </div>
+</div>
+
+                </div>
+
+                <?php endwhile; ?>
+                <!-- Loop End -->
+                <?php wp_reset_postdata(); ?>
+
+            </div>
+
+        </div>
+    </div>
+
+
+    <script>
+        jQuery(document).ready(function () {
+            jQuery(".owl-carousel-blog").owlCarousel({
+                loop: true,
+                margin: 35,
+                responsiveClass: true,
+                nav: true,
+                dots: false,
+                autoplay: false,
+                responsive: {
+                    0: {
+                        items: 1,
+                    },
+                    600: {
+                        items: 2,
+                    },
+                    1000: {
+                        items: 3,
+                    }
+                }
+            });
+        });
+
+    </script>
--- WP PHP CODE/webinar-template.php
+++ WP PHP CODE/webinar-template.php
@@ -6,7 +6,7 @@
         'post_status' => array('publish'),
         'orderby' => 'meta_value_num',
         'order' => 'DESC',
-        'posts_per_page' => 1,
+        'posts_per_page' => -1,
         'meta_query' => array(
             array(
                 'key' => 'select_date',
@@ -21,7 +21,7 @@
 		'post_status' => array('publish'),
 		'orderby' => 'meta_value_num',
 		'order' => 'DESC',
-		'posts_per_page' => 1,
+		'posts_per_page' => -1,
 		'meta_query' => array(
 			array(
 				'key' => 'select_date',
@@ -34,140 +34,161 @@
     $q = new WP_Query($mainarray);
 	$ps = new WP_Query($argsarray);
 ?>
+	
+    <div class="webinar-main-wrapper">
+        <div class="webinar-container">
+            <div class="webinar-carosuel owl-carousel">
+                <!-- Loop Start -->
+				
+                <?php 
+                    if($q->have_posts()): 
+                        while ($q->have_posts()) : 
+                            $q->the_post(); 
+                ?>
+				<div class="slide">
+                    <div class="webinar-content-row">
+                        <div class="webinar-inner">
+                            <div class="uw-title">
+                                <div class="table-title-wrapper">
+                                    <p class="table-title">Presentation Title</p>
+                                </div>
+                                <div class="uw-content">
+                                    <h4 class="upcoming-webinar-haeding"><a href="<?php the_permalink(); ?>">
+                                            <?php the_title(); ?>
+                                        </a>
+									</h4>
+                                    <p>
+                                        <?php if(!empty(get_the_excerpt())){
+                                            echo strip_tags(substr(get_the_excerpt(), 0, 200));
+                                        }
+                                    else{
+                                        echo strip_tags(substr(get_the_content(), 0, 200));
+                                    } ?>
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="uw-date">
+                                <div class="table-title-wrapper">
+                                    <p class="table-title">Date & Time</p>
+                                </div>
+                                <div class="uw-content">
+                                    <p class="table-text">TBD 2023</p>
+                                </div>
+                            </div>
+                            <div class="uw-text">
+                                <div class="table-title-wrapper">
+                                    <p class="table-title">Presenter | Company</p>
+                                </div>
+                                <div class="uw-content">
+                                    <p class="table-text">
+                                        <?php the_field('presenter_name'); ?>
+                                        <?php the_field('presenter_title'); ?>
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="uw-btn">
+                                <div class="table-title-wrapper">
+                                    <p class="table-title"></p>
+                                </div>
+                                <div class="uw-content">
+                                    <p class="table-btn"><a href="<?php the_permalink(); ?>">Register ➜</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-<div class="upcoming-webinar">
-    <table class="webinar-table hp-upcoming-webinar-table ">
-        <thead>
-            <tr>
-                <th class="table-title">Presentation Title</th>
-                <th class="table-title">Date & Time</th>
-                <th class="table-title">Presenter | Company</th>
-				<th class="table-title"></th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php if($q->have_posts()): while ($q->have_posts()) : $q->the_post(); ?>
-            <tr>
-                <td class="uw-content">
-                    <h4 class="upcoming-webinar-haeding"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-                    <p>
-                        <?php if(!empty(get_the_excerpt())){
-                            echo strip_tags(substr(get_the_excerpt(), 0, 200));
-                        }
-                        else{
-                            echo strip_tags(substr(get_the_content(), 0, 200));
-                        } ?>
-                    </p>
-                </td>
-                <?php 
-                    $date = date_create(get_field('select_date'));
-                    
+                <?php endwhile; 
+				
+				elseif($ps->have_posts()): 
+                    while ($ps->have_posts()) : 
+                        $ps->the_post(); 
                 ?>
-                <td class="table-text">TBD 2023</td>
-                <td class="table-text"><?php the_field('presenter_name'); ?>
-					<?php the_field('presenter_title'); ?>
-				</td>
-                <td class="table-btn"><a href="<?php the_permalink(); ?>">Register ➜</a></td>
-            </tr>
-            <?php endwhile; 
-				elseif($ps->have_posts()): while ($ps->have_posts()) : $ps->the_post(); ?>
-            <tr>
-                <td class="uw-content">
-                    <h4 class="upcoming-webinar-haeding"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-                    <p>
-                        <?php if(!empty(get_the_excerpt())){
-                            echo strip_tags(substr(get_the_excerpt(), 0, 200));
-                        }
-                        else{
-                            echo strip_tags(substr(get_the_content(), 0, 200));
-                        } ?>
-                    </p>
-                </td>
-                <?php 
-                    $date = date_create(get_field('select_date'));
-                    
-                ?>
-                <td class="table-text">TBD 2023</td>
-                <td class="table-text"><h4 class="webinar-pn">
-						<?php the_field('presenter_name'); ?>
-						</h4>
-						<p class="webinar-pt">
-							<?php the_field('presenter_title'); ?>
-						</p>
-						<p class="webinar-pc">
-							<?php the_field('presenter_company'); ?>
-						</p>	</td>
-                <td class="table-btn"><a href="<?php the_permalink(); ?>">Register ➜</a></td>
-            </tr>
-            <?php endwhile; else: ?>
-            <tr>
-                <th class="table-record-heading">No Record Found!</th>
-                <script>jQuery(document).ready(function(){ jQuery(document).find('.custom-btn').hide(); });</script>
-            </tr>
+                <div class="slide">
+                    <div class="webinar-content-row">
+                        <div class="webinar-inner">
+                            <div class="uw-title">
+                                <div class="table-title-wrapper">
+                                    <p class="table-title">Presentation Title</p>
+                                </div>
+                                <div class="uw-content">
+                                    <h4 class="upcoming-webinar-haeding"><a href="<?php the_permalink(); ?>">
+                                            <?php the_title(); ?>
+                                        </a></h4>
+                                    <p>
+                                        <?php if(!empty(get_the_excerpt())){
+                                            echo strip_tags(substr(get_the_excerpt(), 0, 200));
+                                        }
+                                    else{
+                                        echo strip_tags(substr(get_the_content(), 0, 200));
+                                    } ?>
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="uw-date">
+                                <div class="table-title-wrapper">
+                                    <p class="table-title">Date & Time</p>
+                                </div>
+                                <div class="uw-content">
+                                    <p class="table-text">TBD 2023</p>
+                                </div>
+                            </div>
+                            <div class="uw-text">
+                                <div class="table-title-wrapper">
+                                    <p class="table-title">Presenter | Company</p>
+                                </div>
+                                <div class="uw-content">
+                                    <p class="table-text">
+                                        <?php the_field('presenter_name'); ?>
+                                        <?php the_field('presenter_title'); ?>
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="uw-btn">
+                                <div class="table-title-wrapper">
+                                    <p class="table-title"></p>
+                                </div>
+                                <div class="uw-content">
+                                    <p class="table-btn"><a href="<?php the_permalink(); ?>">Register ➜</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <?php endwhile; ?>
+            </div>
+            <?php else: ?>
+            <div class="table-record-wrapper">
+                <div class="table-record-heading">No Record Found!</div>
+                <script>jQuery(document).ready(function () { jQuery(document).find('.custom-btn').hide(); });</script>
+            </div>
             <?php endif; wp_reset_postdata(); ?>
-        </tbody>
-    </table>
-</div>
-
-
-
-<!-- ============ -->
-
-
-
-
-<?php if($q->have_posts()): while ($q->have_posts()) : $q->the_post(); ?>
-            <tr>
-                <td class="uw-content">
-                    <h4 class="upcoming-webinar-haeding"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-                    <p>
-                        <?php if(!empty(get_the_excerpt())){
-                            echo strip_tags(substr(get_the_excerpt(), 0, 200));
-                        }
-                        else{
-                            echo strip_tags(substr(get_the_content(), 0, 200));
-                        } ?>
-                    </p>
-                </td>
-                <?php 
-                    $date = date_create(get_field('select_date'));
-                    
-                ?>
-                <td class="table-text">TBD 2023</td>
-                <td class="table-text"><?php the_field('presenter_name'); ?>
-					<?php the_field('presenter_title'); ?>
-				</td>
-                <td class="table-btn"><a href="<?php the_permalink(); ?>">Register ➜</a></td>
-            </tr>
-            <?php endwhile; 
-				elseif($ps->have_posts()): while ($ps->have_posts()) : $ps->the_post(); ?>
-
-
-
-<div class="webinar-content-row">
-    <div class="webinar-inner">
-        <div class="uw-content">
-            <h4 class="upcoming-webinar-haeding"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-            <p>
-                <?php if(!empty(get_the_excerpt())){
-                    echo strip_tags(substr(get_the_excerpt(), 0, 200));
-                }
-                else{
-                    echo strip_tags(substr(get_the_content(), 0, 200));
-                } ?>
-            </p>
+            <!-- Loop End -->
         </div>
-        <div class="uw-date">
-            <p class="table-text">TBD 2023</p>
-        </div>
-        <div class="uw-text">
-            <p class="table-text"><?php the_field('presenter_name'); ?>
-					<?php the_field('presenter_title'); ?>
-            </p>
-        </div>
-        <div class="uw-btn">
-            <p class="table-btn"><a href="<?php the_permalink(); ?>">Register ➜</a></p>
-        </div>
-
     </div>
-</div>
+
+
+    <script>
+        jQuery(document).ready(function () {
+            jQuery(".webinar-carosuel").owlCarousel({
+                loop: false,
+                margin: 35,
+                responsiveClass: true,
+                nav: true,
+                dots: false,
+                autoplay: false,
+                responsive: {
+                    0: {
+                        items: 1,
+                    },
+                    600: {
+                        items: 1,
+                    },
+                    1000: {
+                        items: 1,
+                    }
+                }
+            });
+        });
+
+    </script>
