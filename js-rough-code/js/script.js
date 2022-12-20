c36b38fb4a2890fc2e51d788eca30182122eb0c7
muheeb hussain <muheebhussain14@gmail.com>
2022-12-20 18:15:03 +0500

    12/20

--- js-rough-code/js/script.js
+++ js-rough-code/js/script.js
@@ -1,1 +1,11 @@
-document.querySelector("button").innerHTML = "updated text";
+var world = "12!";
+var text1 = "What a very ";
+text1 += "nice day";
+
+// document.getElementById("heading").innerHTML = "updated text";
+
+document.getElementById("heading").innerHtml = "hello" + world;
+
+document.getElementById("text2").innerHTML = (5 + 6) * 10;
+
+document.getElementById("text1").innerHTML = text1;
